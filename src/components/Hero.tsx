e8f8656b457da5673a20b8b077f489b8cceecc49
Webster Navarro <nwebzx@gmail.com>
2025-08-12 14:40:32 +0800

    fix: adjust max-width and heading styles in Hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,7 +3,7 @@ import { Button } from "./ui/button";
 const Hero = () => {
   return (
     <section id="hero" className="relative grid w-full min-h-screen pb-8">
-      <div className="relative z-0 w-full max-w-screen-2xl h-full mx-auto px-6 md:px-10">
+      <div className="relative z-0 w-full max-w-[1680px] h-full mx-auto px-6 md:px-10">
         <div
           className="relative flex h-[calc(84vh-64px)] w-full items-center justify-center bg-cover bg-center rounded-3xl"
           style={{
@@ -12,13 +12,13 @@ const Hero = () => {
           }}
         >
           <div className="relative max-w-2xl">
-            <div className="flex flex-col items-center text-center text-white font-headline-sans font-semibold text-5xl lg:text-6xl xl:text-7xl tracking-tightest mb-3 !leading-custom ">
-              <p className="text-white text-5xl font-semibold leading-tight mb-4">
+            <div className="flex flex-col items-center text-center text-white font-semibold tracking-tight mb-3 p-1">
+              <p className="text-white font-semibold leading-tight mb-4 text-2xl sm:text-4xl md:text-5xl">
                 Shine as lights in the world, holding forth the{" "}
                 <span className="text-[#ad945e]">Word</span> of life
               </p>
 
-              <h1 className="text-white text-2xl">Philippians 2:15-16</h1>
+              <p className="text-white text-xl sm:text-2xl">Philippians 2:15-16</p>
 
               <div className="mt-10">
                 <Button
