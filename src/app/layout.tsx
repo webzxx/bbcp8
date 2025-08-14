b0c78238bc03acb0a184a2654a529b2393255d77
Webster Navarro <nwebzx@gmail.com>
2025-08-14 17:01:03 +0800

    feat: added footer section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Antonio } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Layout/Header";
+import Footer from "@/components/Home/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -35,6 +36,7 @@ export default function RootLayout({
       >
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
---
+++ src/components/Home/Footer.tsx
@@ -0,0 +1,56 @@
+import Link from "next/link";
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="w-full overflow-x-hidden bg-[#e2e3da] pt-10 lg:pt-20 text-black">
+      <div className="fluid-container">
+        <div className="flex flex-col lg:flex-row lg:justify-between items-center lg:items-start my-8 md:my-16 lg:space-x-8 space-y-16 lg:space-y-0">
+          <div className="flex flex-col">
+            <div className="max-w-xs">
+              <p className="text-gray-800 leading-snug text-lg tracking-wide mb-6 opacity-60">
+                Stay up to date with the Church.
+              </p>
+
+              <h6 className="text-current font-headline-sans font-bold text-xl lg:text-2xl tracking-tight leading-tighter ">
+                Sign up for email updates from Bible Baptist Church
+              </h6>
+            </div>
+
+            <div className="w-auto md:w-96"></div>
+          </div>
+
+          <div className="flex-1 flex flex-col sm:flex-row sm:justify-between lg:justify-end w-full sm:w-auto max-w-sm sm:max-w-full mx-auto sm:space-x-8 xl:space-x-24 space-y-10 sm:space-y-0"></div>
+        </div>
+
+        <div className="flex flex-col xl:flex-row xl:justify-between items-center w-full pt-12 xl:space-x-12 space-y-8 xl:space-y-0">
+          <div className="flex flex-col xl:flex-row xl:space-x-12 space-y-2 xl:space-y-0">
+            <span className="text-sm text-center opacity-60">
+              {" "}
+              © 2025 Bible Baptist Church{" "}
+            </span>
+
+            <div className="flex flex-col sm:flex-row sm:space-x-10">
+              <Link
+                href="/"
+                className="text-sm text-center sm:text-right opacity-60 hover:opacity-100"
+              >
+                Privacy Policy
+              </Link>
+              <Link
+                href="/"
+                className="text-sm text-center sm:text-right opacity-60 hover:opacity-100"
+              >
+                Terms + Conditions
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="w-full mt-12 lg:mt-32 text-white"></div>
+    </footer>
+  );
+};
+
+export default Footer;
