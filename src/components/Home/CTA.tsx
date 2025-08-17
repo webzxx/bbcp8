9d9caec172aed945c5640bc568de2a1853520787
Webster Navarro <nwebzx@gmail.com>
2025-08-17 20:41:33 +0800

    modified: redesign the typography

--- src/components/Home/CTA.tsx
+++ src/components/Home/CTA.tsx
@@ -6,8 +6,8 @@ const CTA = () => {
   return (
     <section id="cta" className="fluid-container relative">
       <div className="bg-[#e2e3da] rounded-3xl px-6 sm:px-12 lg:px-0 flex flex-col-reverse lg:flex-row justify-center items-start">
-        <div className="w-full lg:w-9/20 relative lg:-translate-y-10 pb-10 lg:pb-0">
-          <h1 className="text-current font-headline-sans font-bold text-6xl lg:text-8xl xl:text-10xl leading-none tracking-tightest mb-3">
+        <div className="w-full lg:w-9/20 relative lg:-translate-y-10 pb-10 lg:pb-0 ">
+          <h1 className="text-current font-headline-sans font-bold text-6xl lg:text-8xl xl:text-10xl tracking-tighter leading-none mb-3">
             Join Us
           </h1>
 
@@ -17,7 +17,7 @@ const CTA = () => {
               <em>Onsite</em>
             </h2>
 
-            <p className="text-black leading-snug text-lg tracking-wide mt-0 mb-8 last:mb-0 prose max-w-full lg:max-w-sm">
+            <p className="text-black leading-snug text-lg tracking-tighter mt-0 mb-8 last:mb-0 prose max-w-full lg:max-w-sm">
               Join us every Sunday at 9:30am for our Sunday Service Gatherings
               at Bible Baptist Church, Project 8, Quezon City.
             </p>
@@ -25,7 +25,7 @@ const CTA = () => {
             <div className="relative inline-flex">
               <Button
                 variant="default"
-                className="inline-flex bg-sky-400 text-black hover:bg-blue-800 font-sans leading-none text-lg tracking-tight py-7 px-14 rounded-4xl items-center justify-center gap-4 transition whitespace-nowrap select-none cursor-pointer"
+                className="inline-flex bg-sky-400 text-black hover:bg-blue-800 font-sans leading-none text-lg tracking-tighter py-7 px-14 rounded-4xl items-center justify-center gap-4 transition whitespace-nowrap select-none cursor-pointer"
               >
                 Watch Online
               </Button>
