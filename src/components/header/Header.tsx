3e718568d6b6842d0ca6871437dab462971577d7
Webster Navarro <nwebzx@gmail.com>
2025-08-17 20:50:52 +0800

    fixed: imported DropdownMenu

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -6,6 +6,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { Button } from "../ui/button";
 import { Menu } from "lucide-react";
+import DropdownMenu from "./DropdownMenu"
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
