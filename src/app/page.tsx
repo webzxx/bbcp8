2338fafd2c6ce39b38b16ed89d9ce7d145f17ef1
Webster Navarro <nwebzx@gmail.com>
2025-08-17 21:04:28 +0800

    installed: lenis scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { useEffect } from "react";
+import Lenis from 'lenis';
 import CTA from "@/components/Home/CTA";
 import Giving from "@/components/Home/Giving";
 import Hero from "@/components/Home/Hero";
@@ -5,6 +9,17 @@ import Knowing from "@/components/Home/Knowing";
 import Leadership from "@/components/Home/Leadership";
 
 export default function Home() {
+    useEffect( () => {
+    const lenis = new Lenis()
+
+    function raf(time: number) {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+
+    requestAnimationFrame(raf)
+  }, [])
+
   return (
     <main className="flex flex-col items-center justify-between relative -mb-px">
       <Hero />
