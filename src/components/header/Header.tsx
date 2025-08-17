741761c1caf45a02405899fac494ffc106ac5b70
Webster Navarro <nwebzx@gmail.com>
2025-08-17 20:38:37 +0800

    continued: finishing the design

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,7 +2,6 @@
 
 import React, { useState } from "react";
 import { NavMenu } from "./NavMenu";
-import DropdownMenu from "./DropdownMenu";
 import Link from "next/link";
 import Image from "next/image";
 import { Button } from "../ui/button";
