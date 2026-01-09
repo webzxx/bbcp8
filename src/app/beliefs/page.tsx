e5e18e243afd8c834fe44af939c9c202fe3fc56d
Webster Navarro <nwebxz@gmail.com>
2026-01-09 21:08:12 +0800

    fix: capitalize component name

--- src/app/beliefs/page.tsx
+++ src/app/beliefs/page.tsx
@@ -1,7 +1,7 @@
 export default function OurBeliefs() {
   return (
     <section id="hero" className="relative grid w-full min-h-screen pt-44">
-      <div></div>
+      <div className=""></div>
     </section>
   );
 }
