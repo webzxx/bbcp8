c8c0a0cdd75493f351e347445d00199fa27b0f2a
Webster Navarro <nwebzx@gmail.com>
2026-01-09 21:17:12 +0800

    chore: add placeholder div

--- src/app/beliefs/page.tsx
+++ src/app/beliefs/page.tsx
@@ -1,7 +1,9 @@
 export default function OurBeliefs() {
   return (
     <section id="hero" className="relative grid w-full min-h-screen pt-44">
-      <div className=""></div>
+      <div className="flex flex-col-reverse lg:flex-row gap-6 justify-between">
+        
+      </div>
     </section>
   );
 }
