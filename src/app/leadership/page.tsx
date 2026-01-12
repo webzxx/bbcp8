0b744886dd94da30797ed42f3937a52de925ed48
Webster Navarro <nwebzx@gmail.com>
2026-01-12 17:28:44 +0800

    feat: add leadership page component

---
+++ src/app/leadership/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function OurLeadership() {
+  return (
+    <section id="hero" className="fluid-container">
+      
+    </section>
+  );
+}
