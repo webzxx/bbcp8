9b6bfdccc3c40293f8b28eff9f22d42acd54ad3a
Webster Navarro <nwebzx@gmail.com>
2026-02-05 15:29:22 +0900

    feat: add initial layout for Give page

--- src/app/give/page.tsx
+++ src/app/give/page.tsx
@@ -48,6 +48,77 @@ export default function Give() {
           </div>
         </div>
       </div>
+
+      <main className="pt-20 sm:pt-24 xl:pt-32 pb-20 sm:pb-24 xl:pb-32 relative outer-grid -mb-px">
+        <section>
+          <div className="flex flex-col gap-8 lg:gap-16 lg:flex-row items-start lg:items-center">
+            <div className="relative w-full overflow-hidden rounded-xl lg:w-9/20 aspect-square">
+              <div className="absolute inset-0 h-full w-full">
+                <Image
+                  src="/give-why.jpg"
+                  alt="Why We Give"
+                  fill
+                  className="absolute inset-0 w-full h-full object-cover"
+                  priority
+                />
+              </div>
+            </div>
+
+            <div className="relative space-y-6 lg:w-11/20 tracking-tight">
+              <p className=" text-current font-headline-sans font-bold text-5xl lg:text-6xl xl:text-8xl tracking-tighter mb-5 leading-negative">
+                Why We Give
+              </p>
+
+              <p className="leading-normal text-base tracking-wide max-w-lg"></p>
+
+              <p>
+                At Bible Baptist Church, we don’t say, “we’re going to take the
+                offering”. Simply stated: God is not a taker. God is a giver. So
+                we give the offering. Here’s the truth: there’s nothing you and
+                I have that God needs.
+              </p>
+
+              <p>God doesn’t want to take the offering.</p>
+
+              <p>God doesn’t want to take our money.</p>
+
+              <p>Because, God doesn’t need our money.</p>
+
+              <p>
+                On the other hand, God has let go of everything. He sent His Son
+                to give us life and breath and freedom. To set us free from our
+                chains, from our bondage, from what enslaved us. To deliver us
+                out of death and into life. To give us Himself. So in response
+                to all He’s done we say, “God I want the world to know about
+                Jesus. So I’m going to give into our House. I’m going to give
+                into Bible Baptist Church because I believe here my money will be
+                multiplied to help the city and the world come to know Jesus. At
+                the end of the day, when I stand in heaven I’m going to know
+                that money wasn’t lost. That money was gained because it
+                multiplied itself over and over again into people’s lives.”
+              </p>
+
+              <p>
+                So the prayer is that God would take our hearts today. Capture
+                us today. Awaken us to that fact that He loves us and cares for
+                us. There’s nothing we have that He doesn’t already have a
+                million times over more than we have. And there’s nothing we
+                have that didn’t come from Him first. Then, we will have this
+                great privilege and ability to respond by joining Him with our
+                time, our energy, our gifts, our opportunities, and our money.
+                We have this opportunity to lean into the things that last
+                forever and sewing into what God is doing here in this House.
+              </p>
+
+              <p>For Jesus!</p>
+
+              <p>Ansel</p>
+
+              <p></p>
+            </div>
+          </div>
+        </section>
+      </main>
     </section>
   );
 }
